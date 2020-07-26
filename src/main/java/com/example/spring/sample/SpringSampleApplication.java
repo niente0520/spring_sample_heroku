02e423265c258539a03e8c9ba808844e034a15e4
yuta nisiguchi <yutanisiguchi@yutanoiMac.local>
2020-07-26 14:50:11 +0900

    spring_sample

---
+++ src/main/java/com/example/spring/sample/SpringSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSampleApplication.class, args);
+	}
+
+}
